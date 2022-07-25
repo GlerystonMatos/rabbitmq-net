3411d3bb881d76b4b6fd1ef7548248ec146131c1
Gleryston Matos <glerystonmatos@yahoo.com.br>
2022-07-25 20:25:00 -0300

    Instalação do RabbitMQ.Client no projeto WEB e implementação do envio de mensagens

---
+++ WebApp/WebApp.Api/Controllers/MessageController.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace WebApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private readonly ConnectionFactory _factory;
+
+        //A classe ConnectionFactory é utilizada para configurar a conexão com o RabbitMQ, permitindo criar uma instância de IConnection,
+        //que estabelece a conexão TCP com o broker do RabbitMQ. Finalmente, com essa conexão, é criada um canal (ou channel), que nada
+        //mais é do que uma conexão virtual com o RabbitMQ, utilizando o protocolo AMQP. Múltiplos canais compartilham uma conexão TCP única.
+
+        private const string QUEUE_NAME = "messages";
+
+        public MessageController()
+        {
+            _factory = new ConnectionFactory();
+            _factory.HostName = "10.0.0.131";
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] MessageInputModel message)
+        {
+            IConnection connection = _factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: QUEUE_NAME,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            //O método QueueDeclare cria uma fila, e é independente. Ou seja, ele só vai criar a fila caso ela não exista,
+            //não fazendo nada caso ela já exista.
+
+            //queue: Nome da fila que será criada.
+
+            //durable: Se sim, metadados dela são armazenados no disco e poderão ser recuperados após o reinício do nó do RabbitMQ.
+            //Além disso, em caso de mensagens persistentes, elas são restauradas após o reinício do nó junto a fila durável.
+            //Em caso de uma mensagem persistente em uma fila não-durável, ela ainda será perdida após reiniciar o RabbitMQ.
+
+            //exclusive: Se sim, apenas uma conexão será permitida a ela, e após encerrar, a fila é apagada.
+
+            //autoDelete: Se sim, a fila vai ser apagada caso, após um consumer ter se conectado, todos se desconectaram e ela ficar
+            //sem conexões ativas.
+
+            //Converter mensagem para string
+            string stringFieldMessage = JsonConvert.SerializeObject(message);
+            //Converter string em array de bytes
+            byte[] bytesMessage = Encoding.UTF8.GetBytes(stringFieldMessage);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: QUEUE_NAME,
+                basicProperties: null,
+                body: bytesMessage);
+
+            //O método BasicPublish, realiza a publicação da mensagem. (que está em formato Array de Bytes)
+
+            //exchange: São agentes responsáveis por rotear as mensagens para filas, utilizando atributos de cabeçalho, routing keys, ou bindings.
+
+            //routingKey: Funciona como um relacionamento entre um Exchange e uma fila, descrevendo para qual fila a mensagem deve ser direcionada.
+
+            return Accepted();
+        }
+    }
+}
---
+++ WebApp/WebApp.Api/MessageInputModel.cs
@@ -0,0 +1,13 @@
+namespace WebApp.Api
+{
+    public class MessageInputModel
+    {
+        public int FromId { get; set; }
+
+        public int ToId { get; set; }
+
+        public string? Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
